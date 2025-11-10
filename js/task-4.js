11334deaf6d357a4f78479dd2d7e7fff4eee805b
Alina Kuchma <ostaliva@ukr.net>
2025-11-10 21:22:41 +0200

    added fix version

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,26 @@
 function getShippingCost(country) {
-  const price = 0;
-  if (country === "China") {
-    let price = 100;
-  } else if (country === "Chile") {
-    let price = 250;
-  } else if (country === "Australia") {
-    let price = 170;
-  } else if (country === "Jamaica") {
-    let price = 120;
-  } else {
-    return "Sorry, there is no delivery to your country";
+  let price;
+  switch (country) {
+    case 'China':
+      price = 100;
+      break;
+    case 'Chile':
+      price = 250;
+      break;
+    case 'Australia':
+      price = 170;
+      break;
+    case 'Jamaica':
+      price = 120;
+      break;
+    default:
+      return `Sorry, there is no delivery to your country`;
   }
-
-  `Shipping to ${country} will cost ${price} credits`;
+  return `Shipping to ${country} will cost ${price} credits`;
 }
-
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Germany"));
-console.log(getShippingCost("China"));
-console.log(getShippingCost("Chile"));
-console.log(getShippingCost("Jamaica"));
-console.log(getShippingCost("Sweden"));
+console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
+console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
